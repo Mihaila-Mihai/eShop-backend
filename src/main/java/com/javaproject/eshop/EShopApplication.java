9bb13b6cb41971a8255dc7d8a1201f6d05727290
Mihai Mihaila <mihai.gabriel2000@yahoo.com>
2023-04-09 01:05:30 +0300

    Treated CORS and added Dockerfile

--- src/main/java/com/javaproject/eshop/EShopApplication.java
+++ src/main/java/com/javaproject/eshop/EShopApplication.java
@@ -2,12 +2,26 @@ package com.javaproject.eshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class EShopApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EShopApplication.class, args);
+    }
+
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/eShop/*").allowedOrigins("http://localhost:9000");
+            }
+        };
     }
 
 }
